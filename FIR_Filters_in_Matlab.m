4d9df7921f81519cb02e562311c1737e51d48e41
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2017-12-21 12:37:49 +0300

    Updating *.m file
    Adding license info

--- FIR_Filters_in_Matlab.m
+++ FIR_Filters_in_Matlab.m
@@ -5,6 +5,9 @@
 % MIT License
 % Copyright (c) 2017 Valentyn N Sichkar
 % github.com/sichkar-valentyn
+%
+% ### Reference to:
+% [1] Valentyn N Sichkar. Two-Dimensional Finite Impulse Response (FIR) Filters in Matlab // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Matlab_and_FIR_Filters (date of access: XX.XX.XXXX)
 
 close all;
 
